c18f205d55af44143e11d6c36fcdeff501091fb5
Cameron <cameron@aheadofthecurve.co.za>
2021-01-19 21:00:42 +0200

    Updated the privacy policy.
    - PrivacyPolicyFrag: Updated the privacy policy for weather app.

--- app/src/main/java/za/co/weather/policies/PrivacyPolicyFrag.java
+++ app/src/main/java/za/co/weather/policies/PrivacyPolicyFrag.java
@@ -28,7 +28,7 @@ public class PrivacyPolicyFrag extends Fragment
         View view = inflater.inflate(R.layout.frag_privacy_policy, container, false);
 
         String privacyPolicy =
-                "        <p style=\"text-align:left; margin:4%;\">Your privacy and legal rights are important to us. It is Track My&#39;s policy to respect your privacy regarding any information we may collect from you.<br />\n" +
+                "        <p style=\"text-align:left; margin:4%;\">Your privacy and legal rights are important to us. It is Weather&#39;s policy to respect your privacy regarding any information we may collect from you.<br />\n" +
                 "        <br />\n" +
                 "        We only ask for personal information that is required for us to provide a service to you. We collect it by fair and lawful means, with your knowledge and consent. We also let you know why we&#39;re collecting it and how it will be used.<br />\n" +
                 "        <br />\n" +
@@ -36,19 +36,17 @@ public class PrivacyPolicyFrag extends Fragment
                 "        <br />\n" +
                 "        We don&#39;t share any personally identifying information publicly or with third-parties, except when required to by law.<br />\n" +
                 "        <br />\n" +
-                "        Our website may link to external sites that are not operated by us. Please be aware that we have no control over the content and practices of these sites, and cannot accept responsibility or liability for their respective privacy policies.<br />\n" +
+                "        Our app may link to external sites that are not operated by us. Please be aware that we have no control over the content and practices of these sites, and cannot accept responsibility or liability for their respective privacy policies.<br />\n" +
                 "        <br />\n" +
                 "        You are free to refuse our request for your personal information, with the understanding that we may be unable to provide you with some of your desired services.<br />\n" +
                 "        <br />\n" +
                 "        Your continued use of our app will be regarded as acceptance of our practices around privacy and personal information. If you have any questions about how we handle user data and personal information, feel free to contact us.<br />\n" +
                 "        <br />\n" +
                 "        <b><u>Personal and Sensitive Information Disclosure</u></b><br />\n" +
-                "        To make use of the Track My app please note that we require and store the following information:<br />\n" +
+                "        To make use of the Weather app please note that we require and store the following information:<br />\n" +
                 "        1. Your current location.<br />\n" +
-                "        2. Device IMEI number.<br />\n" +
-                "        3. Device health (Battery life, signal strength).<br />\n" +
                 "        <br />\n" +
-                "        This policy is effective as of 14 November 2020.<br />\n" +
+                "        This policy is effective as of 19 January 2021.<br />\n" +
                 "        <br />\n" +
                 "        By accepting these above terms for personal and sensitivity information disclosure as to how your data will be used you give us permission to save your data for the terms as described above.</p>";
 
